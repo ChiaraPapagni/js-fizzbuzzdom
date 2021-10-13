b1ab5637b5f8b5317940c5d724de695847853432
ChiaraPapagni <chiaraiors@gmail.com>
2021-10-13 14:55:54 +0200

    modificata assegnazione classi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 /* TRACCIA:
-Scrivi un programma che esegua un ciclo da 1 a 100 e ad ogni iterazione appenda un elemento html
-al container con uno stile differente a seconda del valore dell'indice per i multipli di 3,
+Scrivi un programma che esegua un ciclo da 1 a 100 
+ad ogni iterazione appenda un elemento html al container 
+con uno stile differente a seconda del valore dell'indice per i multipli di 3,
 per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5. */
 
 const ul = document.querySelector('ul.list');
@@ -8,17 +9,20 @@ const ul = document.querySelector('ul.list');
 for (let i = 1; i <= 100; i++) {
     //creo nuovo elemento li
     const li = document.createElement('li');
+
+    //assegno a li la classe box
+    li.className = 'box';
+
+    //controllo se i è divisibile per 3,5 o entrambi
+    //in base questo assegno altre classi a li
     if (i % 3 == 0 && i % 5 == 0) {
-        li.className = 'box red';
+        li.className += ' red';
     } else if (i % 5 == 0) {
-        li.className = 'box yellow';
-
+        li.className += ' yellow';
     } else if (i % 3 == 0) {
-        li.className = 'box green';
-
-    } else {
-        li.className = 'box';
+        li.className += ' green';
     }
+
     li.append(i);
     ul.append(li);
 }
