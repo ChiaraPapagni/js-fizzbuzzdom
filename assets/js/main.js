537791ba9cbe2642f684e070dac55fe2fcd92df8
ChiaraPapagni <chiaraiors@gmail.com>
2021-10-13 14:49:51 +0200

    modificato colore in base ai multipli

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,19 @@ per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5. */
 const ul = document.querySelector('ul.list');
 
 for (let i = 1; i <= 100; i++) {
-    //append()
+    //creo nuovo elemento li
     const li = document.createElement('li');
-    li.className = 'box'
+    if (i % 3 == 0 && i % 5 == 0) {
+        li.className = 'box red';
+    } else if (i % 5 == 0) {
+        li.className = 'box yellow';
+
+    } else if (i % 3 == 0) {
+        li.className = 'box green';
+
+    } else {
+        li.className = 'box';
+    }
     li.append(i);
     ul.append(li);
 }
